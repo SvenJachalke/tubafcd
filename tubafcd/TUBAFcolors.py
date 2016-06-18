ff8d1964ca6c7e1c0c57260cabe0d1fb9471ad71
Sven Jachalke <sven.jachalke@physik.tu-freiberg.de>
2016-06-18 21:42:58 +0200

    first module
    __init__.py
    TUBAFcolors.py

---
+++ tubafcd/TUBAFcolors.py
@@ -0,0 +1,33 @@
+#-------------------------------
+#TUBAF CD colors
+#-------------------------------
+
+def TUBAFblue():
+	"""
+	Tuple for TUBAF CD blue color 
+	"""
+	return (0/255.,100/255.,168/255.)
+	
+def TUBAFred():
+	"""
+	Tuple for TUBAF CD red color 
+	"""
+	return (181/255.,18/255.,62/255.)
+def TUBAFgreen():
+	"""
+	Tuple for TUBAF CD green color 
+	"""
+	return (25/255.,150/255.,43/255.)
+def TUBAForange():
+	"""
+	Tuple for TUBAF CD orange color 
+	"""
+	return (244/255.,134/255.,3/255.)
+def TUBAFcyan():
+	"""
+	Tuple for TUBAF CD cyan color 
+	"""
+	return (35/255.,186/255.,226/255.)
+
+#Version of Colors
+TUBAFCDversion='2013'
