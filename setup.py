dc5e0bcadd19dadf9a3ec689255f952a2574c8d1
Sven Jachalke <sven.jachalke@physik.tu-freiberg.de>
2016-06-18 21:43:15 +0200

    setup.py for easy installation

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='tubafcd',
+      version='0.1',
+      description='TU Bergakademie Freiberg Corporate Desing functions',
+      url='',
+      author='Sven Jachalke',
+      author_email='sven.jachalke@physik.tu-freiber.de',
+      license='CC',
+      packages=['tubafcd'],
+      zip_safe=False)
