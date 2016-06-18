e6a0404506e541b4c85984a52fda55345b85521b
Sven Jachalke <sven.jachalke@physik.tu-freiberg.de>
2016-06-18 22:22:19 +0200

    included Readme into installation

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
 from setuptools import setup
 
+def readme():
+	with open('README.rst') as f:
+		return f.read()
+
 setup(name='tubafcd',
-      version='0.2',
+      version='0.2.1',
       description='TU Bergakademie Freiberg Corporate Desing functions',
-      url='',
+      long_description=readme(),
       author='Sven Jachalke',
       author_email='sven.jachalke@physik.tu-freiberg.de',
+	  url='ssh://git@sj-home.ddns.net:/home/git/tubafcd.git',
       license='CC',
       packages=['tubafcd'],
+	  install_requires=[
+	  	'matplotlib',
+	  ],
+	  include_package_data=True,
       zip_safe=False)
